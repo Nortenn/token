37432f2cbb91ab8ca7ea8c78aa8e14f49e3b3b0c
Norton Mourer <137120429+Nortenn@users.noreply.github.com>
2023-07-18 02:33:54 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+let U = 'log'
+    X = 'error'
+
+class Generate {
+    constructor() {
+        this.characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_@!'
+    }
+
+    init(length) {
+        let randomString = '';
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * this.characters.length);
+            randomString += this.characters.charAt(randomIndex);
+        }
+
+        return randomString;
+    }
+}
+
+const generator = new Generate();
+const randomToken = generator.init(64);
+
+console[U](randomToken);
